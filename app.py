b8c2617d3e08d278d47b0bccf71f864229035202
ErnestaMajute <ernesta.majute@gmail.com>
2021-03-23 21:06:18 +0000

    Added Register Template with form

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import (
+    generate_password_hash, check_password_hash)
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -22,6 +25,11 @@ mongo = PyMongo(app)
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
