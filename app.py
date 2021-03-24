4f8a9bb5ee60bc17dff58fde3ef733a22e3a009b
ErnestaMajute <ernesta.majute@gmail.com>
2021-03-24 18:29:39 +0000

    index.html with parallax container added

--- app.py
+++ app.py
@@ -21,10 +21,9 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipes")
-def get_recipes():
-    recipes = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes=recipes)
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
