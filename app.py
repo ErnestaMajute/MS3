8cb79f7544c540e8ed57ae67fafbeb7a54882585
ErnestaMajute <ernesta.majute@gmail.com>
2021-03-26 13:31:40 +0000

    Login template added, styled

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
