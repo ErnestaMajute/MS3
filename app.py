15eb79f2eeb915916492bb7f195a39d2e81c539f
ErnestaMajute <ernesta.majute@gmail.com>
2021-03-28 12:55:54 +0000

    recipes template created

--- app.py
+++ app.py
@@ -83,6 +83,11 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
